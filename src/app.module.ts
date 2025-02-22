0852e189b840cc031d5464dbb4d19a59a2d3d147
elem314 <artmai.pg@gmail.com>
2025-02-22 09:03:05 +0300

    init commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
+import { TelegramModule } from './telegram/telegram.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: Number(process.env.POSTGRES_PORT) || 5432,
+      username: process.env.POSTGRES_USER || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'postgres',
+      database: process.env.POSTGRES_DB || 'chat_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // для разработки, в продакшене лучше отключать
+    }),
+    ChatModule,
+    TelegramModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
